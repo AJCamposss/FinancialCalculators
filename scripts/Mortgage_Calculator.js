59d372fea78ab764a3c0b9f16a34b551b69195bf
AJCamposss <adrian.campos869@gmail.com>
2023-10-23 14:05:13 -0700

    added basic outline and JavaScript

---
+++ scripts/Mortgage_Calculator.js
@@ -0,0 +1,17 @@
+windows.onload = init;
+
+let principal;
+let interestRate;
+let loanLength;
+let monthlyPayment
+let totalInterestPaid
+
+function init()
+{
+    principal = document.getElementById("principal")
+    interestRate = document.getElementById("interest")
+    loanLength = document.getElementById("loanLength")
+
+    
+
+}
