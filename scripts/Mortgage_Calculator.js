f7d49a9cd12902b6d8ee992ab4178ac95e8e1425
AJCamposss <adrian.campos869@gmail.com>
2023-10-23 15:58:36 -0700

    Brain is ready to explode

--- scripts/Mortgage_Calculator.js
+++ scripts/Mortgage_Calculator.js
@@ -1,17 +1,29 @@
-windows.onload = init;
+window.onload = init;
 
 let principal;
 let interestRate;
+let monthlyRate;
 let loanLength;
-let monthlyPayment
-let totalInterestPaid
+let totalMonths;
+
+let paymentsPerMonth;
+let totalInterestPaid;
+// Principal amount + interest rate/ 12) (1 + interest rate / 12)^ -payments
 
 function init()
 {
-    principal = document.getElementById("principal")
-    interestRate = document.getElementById("interest")
-    loanLength = document.getElementById("loanLength")
+    principal = parseFloat(document.getElementById("principal").value)
+    interestRate = parseFloat(document.getElementById("interest").value) 
+    loanLength = parseFloat(document.getElementById("loanLength").value)
+    document.getElementById("submitBtn").onclick = calculate
+   
+}
 
+function calculate()
+{
     
-
+    monthlyRate = interestRate / 1200;
+    totalMonths = loanLength * 12; 
+    paymentsPerMonth = (principal + (monthlyRate)) /  1 - Math.pow(1 + monthlyRate, totalMonths);
+    console.log(interestRate)
 }
